295ea9db0e5a99a95723eb38e0d2710dd9e7d1e5
David Germiquet <david@trulycanadian.net>
2025-08-07 13:01:06 -0400

    Fix online discord with proper  functionality

--- langchaintools.py
+++ langchaintools.py
@@ -1,5 +1,6 @@
 from langchain.agents import AgentType
 from langchain.tools import Tool
+import nextcord
 import datetime
 from langchain_core.messages import HumanMessage,SystemMessage,AIMessage,ToolMessage
 import re
@@ -102,7 +103,7 @@ def get_discord_server_members(bot,_:str) -> str:
     return f'The list of members on this channel on server are {all_guild_members}'
     
 def get_discord_online_server_members(bot,_:str) -> str:
-    guild_members = [ member.name for member in bot['message'].guild.members if member.status == discord.Status.online or discord.Status.dnd or discord.Status.idle ]
+    guild_members = [ member.name for member in bot['message'].guild.members if member.status != nextcord.Status.offline ]
     all_guild_members = " ".join(guild_members)
     return f'The list of members on this channel on server are {all_guild_members}'
 
